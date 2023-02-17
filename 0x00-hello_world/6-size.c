fd76ff2a8633402ed9daf5b45a76e7e977396161
Your Name <you@example.com>
2023-02-17 07:20:12 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* *
+*main - Entry point
+*/
+
+int main (void)
+{
+	printf("size of a char:%zu byte (s)\n", size of (char));
+	printf("size of an int:%zu byte (s)\n", size of (int));
+	printf("size of a long int:%zu byte (s)\n", size of (long int));
+	printf("size of a long long int:%zu byte (s)\n", size of(long long int));
+	printf("size of a float:%zu byte (s)\n", size of(float));
+	return (0);
+}
