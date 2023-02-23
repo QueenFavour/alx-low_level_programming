eafc7f24a0d2164d638772146ac9b7e834c5160f
Your Name <you@example.com>
2023-02-23 04:11:37 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
+
 int i;
 
 printf("Infinite loop incoming :(\n");
-
 i = 0;
 
 /*while(i < 10)*/
@@ -19,6 +19,5 @@ i = 0;
 /*}*/
 
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
